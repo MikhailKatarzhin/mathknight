5ef1ffb1593accedfaf78c5ba4e196e372f870d9
VladimirSolodilov <vvsolodilov001@gmail.com>
2021-06-03 22:34:28 +0400

    Веб-приложение
    Создание структуры веб-приложения и его соединение с БД

---
+++ MathKnightApplication/src/main/java/com/example/mathknight/MathKnightApplication.java
@@ -0,0 +1,13 @@
+package com.example.mathknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathKnightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathKnightApplication.class, args);
+    }
+
+}
---
+++ MathKnightApplication/src/test/java/com/example/mathknight/MathKnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mathknight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathKnightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
